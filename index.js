0e40c40307fde62283c09c04734a622ca21103e0
himel1625 <himelmia1625@gmail.com>
2025-01-06 18:56:18 +0600

    Added all File

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const port = process.env.PORT || 4000;
 const app = express();
 // middleware
-app.use(cors());
+app.use(cors('*'));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.s9ap0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -31,58 +31,50 @@ async function run() {
     });
 
     app.get('/products', async (req, res) => {
-      const cursor = SportsCollection.find().limit(6);
-      const result = await cursor.toArray();
+      // const page = parseInt(req.query.page);
+      // const size = parseInt(req.query.size);
+      // .skip(page * size)
+      //   .limit(size)
+      // console.log(page, size);
+      const result = await SportsCollection.find().toArray();
       res.send(result);
     });
 
-    app.put('/AllProducts/:id', async (req, res) => {
-      const id = req.params.id;
-      const filter = { _id: new ObjectId(id) };
-      const options = { upsert: true };
-      const updateDoc = req.body;
-      const updateProducts = {
-        $set: {
-          image: updateDoc.image,
-          itemName: updateDoc.itemName,
-          category: updateDoc.category,
-          description: updateDoc.description,
-          price: updateDoc.price,
-          rating: updateDoc.rating,
-          customization: updateDoc.customization,
-          processingTime: updateDoc.processingTime,
-          stockStatus: updateDoc.stockStatus,
-        },
-      };
-      const result = await SportsCollection.updateOne(
-        filter,
-        updateProducts,
-        options
-      );
-      res.send(result);
-    });
-    
-    app.get('/AllProducts', async (req, res) => {
-      const cursor = SportsCollection.find();
-      const result = await cursor.toArray();
-      res.send(result);
-    });
+    // app.put('/AllProducts/:id', async (req, res) => {
+    //   const id = req.params.id;
+    //   const filter = { _id: new ObjectId(id) };
+    //   const options = { upsert: true };
+    //   const updateDoc = req.body;
+    //   const updateProducts = {
+    //     $set: {
+    //       image: updateDoc.image,
+    //       itemName: updateDoc.itemName,
+    //       category: updateDoc.category,
+    //       description: updateDoc.description,
+    //       price: updateDoc.price,
+    //       rating: updateDoc.rating,
+    //       customization: updateDoc.customization,
+    //       processingTime: updateDoc.processingTime,
+    //       stockStatus: updateDoc.stockStatus,
+    //     },
+    //   };
+    //   const result = await SportsCollection.updateOne(
+    //     filter,
+    //     updateProducts,
+    //     options,
+    //   );
+    //   res.send(result);
+    // });
+
     app.get('/Details', async (req, res) => {
       const cursor = SportsCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
 
-    app.delete('/AllProducts/:id', async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await SportsCollection.deleteOne(query);
-      res.send(result);
-    });
-
-    // console.log(
-    //   'Pinged your deployment. You successfully connected to MongoDB!'
-    // );
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!',
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
